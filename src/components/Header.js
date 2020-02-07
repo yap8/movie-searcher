19a353a9d1904facab4c48e825e72d0e28050e81
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-02-08 01:30:23 +0300

    added styles for Header and Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Nav from './Nav'
+import styled from 'styled-components'
+
+const Container = styled.header`
+  background-color: #2c2e37;
+  min-height: 64px;
+  padding: 8px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 const Header = () => {
   return (
-    <header>
+    <Container>
       <Nav />
-    </header>
+    </Container>
   )
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,43 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+
+import styled from 'styled-components'
+
+const Container = styled.nav`
+
+`
+
+const List = styled.ul`
+  display: flex;
+`
+
+const Item = styled.li`
+  &:not(:last-child) {
+    margin-right: 20px;
+  }
+`
+
+const Link = styled(NavLink)`
+  padding: 4px;
+  color: #fff;
+  font-size: 2rem;
+  &:hover {
+    text-decoration: underline;
+  }
+` 
 
 const Nav = () => {
   return (
-  <nav>
-    <ul>
-      <li>
+  <Container>
+    <List>
+      <Item>
         <Link to="/">Home</Link>
-      </li>
-      <li>
+      </Item>
+      <Item>
         <Link to="/about">About</Link>
-      </li>
-    </ul>
-  </nav>
+      </Item>
+    </List>
+  </Container>
   )
 }
 
